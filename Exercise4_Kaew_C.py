8a3989cbbd175ff51a0a9902d0b00e535beabe09
galzchan <c.sathita@gmail.com>
2020-08-17 16:53:32 +0700

    Create Exercise4_Kaew_C.py

---
+++ Exercise4_Kaew_C.py
@@ -0,0 +1,12 @@
+FE = 80.5
+GB = 70
+ITC = 72
+CP =85
+
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ITC)
+print("Computer Programming : ",CP)
+
+
