3945ac6c4d33016c2df9f3036a085be09078c999
galzchan <c.sathita@gmail.com>
2020-08-31 14:29:37 +0700

    Create FunctionExample.py

---
+++ FunctionExample.py
@@ -0,0 +1,25 @@
+# def sayHelloWorld():
+#     print("Hello World")
+#     myName()
+#     print("Woooooo!!")
+# def myName():
+#     print("Kaew Chan")
+#     print("HaHaaa")
+# sayHelloWorld()
+# myName()
+
+def addNumber(x,y):
+    print(x+y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x-y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x*y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x/y)
+addNumber(100,25)
