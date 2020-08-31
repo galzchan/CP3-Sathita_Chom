6384e906731ae67390c625bb7833cc3868eb3e72
galzchan <c.sathita@gmail.com>
2020-08-31 13:30:02 +0700

    Create Exercise11_Sathita_C.py
    การบ้านพีระมิด

---
+++ Exercise11_Sathita_C.py
@@ -0,0 +1,11 @@
+# NumInput = int(input("Put a number : "))
+# for i in range(NumInput):
+#     print((" "*(NumInput+1-i))+("*"*((2*i)-1)))
+
+
+
+
+
+number = int(input("ความสูงของพีระมิด: "))
+for i in range(number):
+    print((i+1)*"*")
